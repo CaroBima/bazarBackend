6803532e1ecb98fb41fdeac8a7e14ea5c0112e99
CaroBima <82395515+CaroBima@users.noreply.github.com>
2022-06-14 19:23:38 -0300

    implementacion de los metodos de ClienteService

--- src/main/java/com/bazar/bazar/service/ClienteService.java
+++ src/main/java/com/bazar/bazar/service/ClienteService.java
@@ -2,7 +2,9 @@
 package com.bazar.bazar.service;
 
 import com.bazar.bazar.model.Cliente;
+import com.bazar.bazar.repository.IClienteRepository;
 import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 /**
@@ -12,29 +14,54 @@ import org.springframework.stereotype.Service;
 @Service
 public class ClienteService implements IClienteService {
 
+    @Autowired
+    private IClienteRepository clienteRepo;
+        
+    //Guardar un nuevo cliente
     @Override
-    public void agregarCliente(Cliente cliente) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    public void guardarCliente(Cliente cliente) {
+        clienteRepo.save(cliente);
     }
 
+    //devuelve todos los clientes
     @Override
     public List<Cliente> buscarClientes() {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+       return clienteRepo.findAll();
     }
 
+    //Permite buscar un cliente por el id y si no existe devuelve null
     @Override
     public Cliente buscarUnCliente(Long idCliente) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        return clienteRepo.findById(idCliente).orElse(null);
     }
 
+    //permite modificar uno o varios valores del cliente
     @Override
-    public Cliente modificarCliente(Long idCliente) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    public Cliente modificarCliente(Long idCliente, String nuevoNombre, String nuevoApellido, String nuevoDni) {
+    
+        Cliente cliente = this.buscarUnCliente(idCliente);
+                
+        //verifico que se haya pasado este parametro para no guardar un valor null en la bbdd
+        if(nuevoApellido != null){
+           cliente.setApellido(nuevoApellido);
+       }
+        
+        if(nuevoNombre != null){
+            cliente.setNombre(nuevoNombre);
+        }
+            
+        if(nuevoDni != null){
+            cliente.setDni(nuevoDni);
+        }
+        
+       this.guardarCliente(cliente);
+       return cliente;
     }
 
+    //permite eliminar fisicamente al cliente de la bd
     @Override
     public void eliminarCliente(Long idCliente) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        clienteRepo.deleteById(idCliente);
     }
 
  
--- src/main/java/com/bazar/bazar/service/IClienteService.java
+++ src/main/java/com/bazar/bazar/service/IClienteService.java
@@ -9,9 +9,9 @@ import java.util.List;
  * @author Caro
  */
 public interface IClienteService {
-    public void agregarCliente(Cliente cliente);
+    public void guardarCliente(Cliente cliente);
     public List<Cliente> buscarClientes();
     public Cliente buscarUnCliente(Long idCliente);
-    public Cliente modificarCliente(Long idCliente);
+    public Cliente modificarCliente(Long idCliente, String nuevoNombre, String nuevoApellido, String nuevoDni);
     public void eliminarCliente(Long idCliente);
 }
