366cb55b162f941c30cadb3d71f7886a64c3229c
CaroBima <82395515+CaroBima@users.noreply.github.com>
2022-06-15 19:34:00 -0300

    Implementando metodos del servicio venta, falta solo el de modificar venta

--- src/main/java/com/bazar/bazar/service/IVentaService.java
+++ src/main/java/com/bazar/bazar/service/IVentaService.java
@@ -2,7 +2,10 @@
 package com.bazar.bazar.service;
 
 
+import com.bazar.bazar.model.Cliente;
+import com.bazar.bazar.model.Producto;
 import com.bazar.bazar.model.Venta;
+import java.time.LocalDate;
 import java.util.List;
 
 /**
@@ -10,10 +13,10 @@ import java.util.List;
  * @author Caro
  */
 public interface IVentaService {
-    public void agregarCliente(Venta venta);
-    public List<Venta> buscarClientes();
-    public Venta buscarUnCliente(Long idVenta);
-    public Venta modificarCliente(Long idVenta);
-    public void eliminarCliente(Long idVenta);
+    public void guardarVenta(Venta venta);
+    public List<Venta> buscarVentas();
+    public Venta buscarUnaVenta(Long idVenta);
+    public Venta  modificarVenta(Long idVenta, LocalDate nuevaFechaVenta, Double total, List<Producto> listaProd, Cliente uncliente);
+    public void eliminarVenta(Long idVenta);
     
 }
--- src/main/java/com/bazar/bazar/service/VentaService.java
+++ src/main/java/com/bazar/bazar/service/VentaService.java
@@ -1,8 +1,13 @@
 
 package com.bazar.bazar.service;
 
+import com.bazar.bazar.model.Cliente;
+import com.bazar.bazar.model.Producto;
 import com.bazar.bazar.model.Venta;
+import com.bazar.bazar.repository.IVentaRepository;
+import java.time.LocalDate;
 import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 /**
@@ -12,29 +17,36 @@ import org.springframework.stereotype.Service;
 @Service
 public class VentaService implements IVentaService{
 
-    @Override
-    public void agregarCliente(Venta venta) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
-    }
-
-    @Override
-    public List<Venta> buscarClientes() {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
-    }
-
-    @Override
-    public Venta buscarUnCliente(Long idVenta) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
-    }
-
-    @Override
-    public Venta modificarCliente(Long idVenta) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
-    }
-
-    @Override
-    public void eliminarCliente(Long idVenta) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
-    }
+    @Autowired
+    private IVentaRepository ventaRepo;
     
+    @Override
+    public void guardarVenta(Venta venta) {
+        ventaRepo.save(venta);
+    }
+
+    @Override
+    public List<Venta> buscarVentas() {
+        return ventaRepo.findAll();
+    }
+
+    @Override
+    public Venta buscarUnaVenta(Long idVenta) {
+        return ventaRepo.findById(idVenta).orElse(null);
+    }
+
+    @Override
+    public Venta modificarVenta(Long idVenta, LocalDate nuevaFechaVenta, Double total, List<Producto> listaProd, Cliente uncliente) {
+        //implementar metodo para modificar venta
+        Venta venta = new Venta();
+        return venta;
+    }
+
+
+
+    @Override
+    public void eliminarVenta(Long idVenta) {
+        ventaRepo.deleteById(idVenta);
+    }
+
 }
